6b0da95f659a5331cc6c3fed29f7550e24671248
anura <anura@ANURAG>
2023-02-20 22:21:12 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyProfile.Models;
+using System.Diagnostics;
+
+namespace MyProfile.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Skill()
+        {
+            return View();
+        }
+        public ActionResult Service()
+        {
+            return View();
+        }
+        public ActionResult Portfolio()
+        {
+            return View();
+        }
+
+        public ActionResult TESTIMONIAL()
+        {
+            return View();
+        }
+
+        public ActionResult Blog()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+
+
+    }
+}
